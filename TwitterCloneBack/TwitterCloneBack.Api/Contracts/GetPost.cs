dd2f518afb68895faf846e99363de2e2baa4bbf4
generalmaks <schrinan@gmail.com>
2026-04-27 18:12:27 +0300

    Added working main feed on frontend

---
+++ TwitterCloneBack/TwitterCloneBack.Api/Contracts/GetPost.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitterCloneBack.Model.Contracts;
+
+public class GetPost
+{
+    public int Id { get; set; }
+
+    public int AuthorId { get; set; }
+
+    public string TextContent { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+}
