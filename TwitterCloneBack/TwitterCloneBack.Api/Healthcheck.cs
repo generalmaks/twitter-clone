efdd5a46d1ffe4d2297928cca9767503cedf85ea
generalmaks <schrinan@gmail.com>
2026-05-17 19:28:42 +0300

    Added healthcheck controller

---
+++ TwitterCloneBack/TwitterCloneBack.Api/Healthcheck.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterCloneBack;
+
+[ApiController]
+[Route("api/v1/healthcheck")]
+public class Healthcheck : ControllerBase
+{
+    [HttpGet]
+    public Task<ActionResult<ServerHealth>> HealthCheck()
+    {
+        return Task.FromResult<ActionResult<ServerHealth>>(Ok(new ServerHealth(DateTime.UtcNow, "fine")));
+    }
+}
+
+public record ServerHealth(DateTime Time, string Status);
